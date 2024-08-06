5fef61709763caadef85bf3ae7aa51e6d5dd9b25
Martin Daffunchio <mdaffunchio@mpba.gov.ar>
2024-08-06 20:02:38 -0300

    WIP

--- install.sh
+++ install.sh
@@ -18,4 +18,6 @@ zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 zsh -c 'git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions'
 
+git clone --depth 1 https://github.com/romkatv/powerlevel10k "$HOME"/.oh-my-zsh/custom/themes/powerlevel10k
+
 cp ./.zshrc ~
