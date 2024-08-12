4fa36468e1cf3fa304c600fb1559238c797702f9
Martin Daffunchio <mdaffunchio@mpba.gov.ar>
2024-08-12 13:42:51 -0300

    WIP

--- installl.sh
+++ installl.sh
@@ -27,8 +27,12 @@ version_actual=$(zsh --version | cut -d' ' -f2)
 version_deseada="5.7.0"
 if [ "$version_actual" > "$version_deseada" ]; then
   echo "La versión de zsh es la correcta."
+  
   # powerlevel10k Requiere version 5.7
-  git clone --depth 1 https://github.com/romkatv/powerlevel10k "$HOME"/.oh-my-zsh/custom/themes/powerlevel10k
+  if [ ! -d "$HOME"/.oh-my-zsh/custom/themes/powerlevel10k ]; then
+    git clone --depth 1 https://github.com/romkatv/powerlevel10k "$HOME"/.oh-my-zsh/custom/themes/powerlevel10k
+  fi
+    
   cp ./.pk10k.zsh ~
   # update theme
   sed -i '/^ZSH_THEME/c\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
